5b0c2195fa673212cd4f9bdcc186b60a6d16256f
Ashar Farooq <ashar744@gmail.com>
2021-08-11 23:24:34 -0400

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
+import "./App.css";
 import Login from "./components/LogIn.js";
 import LogOut from "./components/LogOut.js";
 
 function App() {
-
   return (
     <div className="App">
-    <h1>Sign In To Your Google Account!</h1>
-    <Login />
-    <LogOut />
+      <h1>Sign In To Your Google Account!</h1>
+      <Login />
+      <LogOut />
     </div>
   );
 }
