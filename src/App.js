a894ee0b81180ebd180f7dd1f1f0155a4e99381f
Ashar Farooq <ashar744@gmail.com>
2021-08-11 23:03:02 -0400

    changing imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import LogIn from "./components/LogIn";
+import Login from "./components/LogIn";
 import LogOut from "./components/LogOut";
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-    <LogIn />
+    <Login />
     <LogOut />
     </div>
   );
